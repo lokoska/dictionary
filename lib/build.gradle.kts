bc436359fbc8633b3918ca23c9d4e3556ef986b4
avdim <avdim@mail.ru>
2020-04-05 23:45:02 +0300

    redux

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -5,8 +5,28 @@ plugins {
 kotlin {
     js {
         browser {
-
+            testTask {
+                testLogging {
+                    showExceptions = true
+//                    exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+                    showCauses = true
+                    showStackTraces = true
+                }
+            }
         }
+        nodejs {
+            testTask {
+                testLogging {
+                    showExceptions = true
+//                    exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+                    showCauses = true
+                    showStackTraces = true
+                }
+            }
+        }
+    }
+    jvm {
+
     }
 
     sourceSets {
@@ -15,9 +35,32 @@ kotlin {
                 implementation(kotlin("stdlib-common"))
             }
         }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
         val jsMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$COROUTINES_VERSION")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-junit"))
             }
         }
     }
